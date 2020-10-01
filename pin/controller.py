aaea432bbad375c329251cc95605f129cff6ab7d
BoBoBo <bobobo@BoBoBos-MacBook-Pro.local>
2020-10-02 00:21:36 +0800

    Save daily.

--- pin/controller.py
+++ pin/controller.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 #BoBoBo#
 
 from functools import wraps
--- pin/embed/handler.py
+++ pin/embed/handler.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
+#BoBoBo#
+
 from http.server import BaseHTTPRequestHandler
 from http import HTTPStatus
 import io
-
-#BoBoBo#
 
 class PinHTTPRequestHandler(BaseHTTPRequestHandler):
 
@@ -10,26 +11,29 @@ class PinHTTPRequestHandler(BaseHTTPRequestHandler):
         self.do_POST()
 
     def do_POST(self):
+        self.pin_request_response()
+
+        self.biz()
+
+        f = io.BytesIO()
+        f.write(self.pin_response.content)
+        f.seek(0)
+        self.send_response(self.pin_response.status)
+
+        for k, v in self.pin_response.headers.items():
+            self.send_header(k, v)
+
+        self.send_header('Content-Length', str(len(content)))
+        self.end_headers()
+        self.wfile.write(bytes(self.pin_response.content, 'utf-8'))
+
+    def pin_request_response(self):
         self.pin_request = {}
         self.pin_request['request'] = self.request
         self.pin_request['headers'] = self.headers
         self.pin_request['method'] = self.command
         self.pin_request['uri'] = self.path
-
         self.pin_response = PinHTTPResponse()
-
-        self.biz()
-
-        enc = "UTF-8"
-        content = self.pin_response.content.encode(enc)
-        f = io.BytesIO()
-        f.write(content)
-        f.seek(0)
-        self.send_response(self.pin_response.status)
-        self.send_header("Content-type", "text/html; charset=%s" % enc)
-        self.send_header("Content-Length", str(len(content)))
-        self.end_headers()
-        self.wfile.write(bytes(self.pin_response.content, 'utf-8'))
 
     def biz(self):
         pass
